31af11526945db799fd0127a35774cf6a2cba260
Carlo Rapisarda <carlora@kth.se>
2019-02-24 19:17:14 +0100

    Improve demo script

--- demo.py
+++ demo.py
@@ -1,7 +1,33 @@
 import gym
+from tqdm import tqdm
 
 from ddpg_agent import DdpgHer
 from train import OUT_DIR
+
+
+def demo(env, agent, steps=10000):
+    obs = env.reset()
+    for _ in range(steps):
+        action = agent.predict(obs)
+        obs, reward, done, _ = env.step(action)
+        env.render()
+        if done:
+            obs = env.reset()
+
+
+def evaluate(env, agent, episodes=200):
+    success_rate = 0.0
+    for _ in tqdm(range(episodes)):
+        obs = env.reset()
+        while True:
+            action = agent.predict(obs)
+            obs, reward, done, info = env.step(action)
+            success = info['is_success']
+            if done or success == 1.0:
+                success_rate += success
+                break
+    success_rate /= episodes
+    print('success_rate', success_rate)
 
 
 def main():
@@ -9,18 +35,12 @@ def main():
     # env = gym.make('FetchPickAndPlace-v1')
     # local_dir = f'{OUT_DIR}/test3'
 
-    env = gym.make('MovingHandReach-v0', ignore_rotation_ctrl=True, ignore_target_rotation=True)
-    local_dir = f'{OUT_DIR}/hand_reach_no_rot'
+    env = gym.make('HandPickAndPlace-v0', ignore_rotation_ctrl=True, ignore_target_rotation=True)
+    local_dir = f'{OUT_DIR}/hand_pick_and_place_obs_ls_no_rot'
 
-    agent = DdpgHer.load(env, local_dir)
+    agent = DdpgHer.load(env, local_dir, epoch=44)
 
-    obs = env.reset()
-    for _ in range(10000):
-        action = agent.predict(obs)
-        obs, reward, done, _ = env.step(action)
-        env.render()
-        if done:
-            obs = env.reset()
+    demo(env, agent)
 
 
 if __name__ == '__main__':
