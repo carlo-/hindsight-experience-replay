c334e1556388e14af0efa3401c49381e9bd47da5
Carlo Rapisarda <carlora@kth.se>
2019-02-27 12:34:14 +0100

    Add function to plot results

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import gym
 from tqdm import tqdm
 
@@ -30,16 +32,43 @@ def evaluate(env, agent, episodes=200):
     print('success_rate', success_rate)
 
 
+def plot_progress(csv_path):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import matplotlib.ticker as ticker
+
+    df = pd.read_csv(csv_path)
+    success_rate = df['test_success_rate'].values
+    steps_per_epoch = df['env_steps'][0]
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(df['training_iteration'].values, success_rate)
+    ax1.set_ylabel('Success rate')
+    ax1.set_xlabel('Epoch ({} env. steps each)'.format(steps_per_epoch))
+
+    ax2 = ax1.twiny()
+    ax2.plot(df['total_time'].values, success_rate).pop(0).remove()
+
+    def format_date(x, pos=None):
+        return str(timedelta(seconds=x))
+
+    ax2.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+    for tick in ax2.get_xticklabels():
+        tick.set_rotation(45)
+
+    plt.show()
+
+
 def main():
 
     # env = gym.make('FetchPickAndPlace-v1')
     # local_dir = f'{OUT_DIR}/test3'
 
-    env = gym.make('HandPickAndPlace-v0', ignore_rotation_ctrl=True, ignore_target_rotation=True)
-    local_dir = f'{OUT_DIR}/hand_pick_and_place_obs_ls_no_rot'
+    env = gym.make('HandPickAndPlace-v0', ignore_rotation_ctrl=True, ignore_target_rotation=True, success_on_grasp_only=True)
+    local_dir = f'{OUT_DIR}/hand_pp_grasp_only/mordor'
 
-    agent = DdpgHer.load(env, local_dir, epoch=44)
-
+    plot_progress(f'{local_dir}/progress.csv')
+    agent = DdpgHer.load(env, local_dir, epoch=100)
     demo(env, agent)
 
 
